bfbbb4e31d5110c721ff0a1e2647f647e8e2beba
Lucas <lucas@Macbook-M1.local>
2023-01-27 17:03:07 -0300

    feat: add automatization to execute project

--- start.sh
+++ start.sh
@@ -5,18 +5,21 @@ docker stop web-chat-frontend
 docker stop web-chat-backend
 docker stop web-chat-frontend-1
 docker stop web-chat-backend-1
+docker stop alpine
 
 echo "Removing containers"
 docker rm -f web-chat-frontend
 docker rm -f web-chat-backend
 docker rm -f web-chat-frontend-1
 docker rm -f web-chat-backend-1
+docker rm -f alpine
 
 echo "Removing images"
 docker image rm -f web-chat-frontend
 docker image rm -f web-chat-backend
 docker image rm -f web-chat-frontend-1
 docker image rm -f web-chat-backend-1
+docker image rm -f alpine
 
 echo "Show current images"
 docker image ls
