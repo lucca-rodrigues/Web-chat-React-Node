d124ea57e418afb572fa052307c0c06a615f2ad0
Lucas <lucas@Macbook-M1.local>
2023-01-27 14:52:49 -0300

    feat: resolve error cors and show messages

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 const http = require("http");
 const socketIo = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
+app.use(
+  cors({
+    origin: "http://localhost:3000", // replace with your React app's origin
+    methods: ["GET", "POST", "PUT", "PATCH", "OPTIONS", "DELETE"],
+  })
+);
 
 io.on("connection", (socket) => {
   console.log("New client connected");
@@ -20,11 +27,11 @@ io.on("connection", (socket) => {
   });
 });
 
-app.get("/", (req, res) => {
-  res.send({
-    response: "Status ok!",
-  });
-});
+// app.get("/", (req, res) => {
+//   res.send({
+//     response: "Status ok!",
+//   });
+// });
 
 server.listen(3333, () => {
   console.log("Server started on port 3333");
