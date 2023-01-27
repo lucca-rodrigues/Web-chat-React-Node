9bf0de19a11e90d72a2c72fa3214049104377be5
Lucas <lucas@Macbook-M1.local>
2023-01-27 16:46:43 -0300

    feat: add docker automatization

--- start.sh
+++ start.sh
@@ -3,14 +3,20 @@
 echo "Stopping docker containers"
 docker stop web-chat-frontend
 docker stop web-chat-backend
+docker stop web-chat-frontend-1
+docker stop web-chat-backend-1
 
 echo "Removing containers"
 docker rm -f web-chat-frontend
 docker rm -f web-chat-backend
+docker rm -f web-chat-frontend-1
+docker rm -f web-chat-backend-1
 
 echo "Removing images"
 docker image rm -f web-chat-frontend
 docker image rm -f web-chat-backend
+docker image rm -f web-chat-frontend-1
+docker image rm -f web-chat-backend-1
 
 echo "Show current images"
 docker image ls
